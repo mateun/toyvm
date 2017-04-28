f857cc133426dec86389bf04e32f18fa3ec47cc9
martingruscher <martingruscher@gmail.com>
2017-04-28 21:12:56 +0200

    remove prints of command impl.

--- src/command_impl.cpp
+++ src/command_impl.cpp
@@ -2,10 +2,8 @@
 #include <iostream>
 
 void VM::e_addi() {
-	std::cout << "addi" << std::endl;
 	ui32 val1 = _stack[_sp--];
 	ui32 val2 = _stack[_sp--];
-	std::cout << "val1 und 2: " << std::to_string(val1) << std::endl;
 	_stack[++_sp] = val1 + val2;
 	_cp++;
 
@@ -14,7 +12,6 @@ void VM::e_addi() {
 void VM::e_subi() {}
 
 void VM::e_consti() {
-	std::cout << "consti" << std::endl;
 	ui32 val = _code[++_cp];
 	_stack[++_sp] = val;
 	_cp++;

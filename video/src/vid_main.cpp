7755745267c4664c4bc5190307eefc6485ddb144
martingruscher <martingruscher@gmail.com>
2017-05-03 20:53:01 +0200

    loading a bitmap, starting to play around with fonts

--- video/src/vid_main.cpp
+++ video/src/vid_main.cpp
@@ -1,16 +1,29 @@
 #include <SDL2/SDL.h>
+#include <string>
+
+SDL_Texture* loadImageIntoTexture(const std::string& imageName, SDL_Renderer* renderer) {
+	SDL_Surface* surface = SDL_LoadBMP(imageName.c_str());
+	if (!surface) { SDL_Log("E: loading image %s %s", imageName.c_str(), SDL_GetError());
+									return nullptr;}
+	SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+	SDL_FreeSurface(surface);
+	return texture;
+}
+
+void fillRect(SDL_Rect& r, int size) { 
+	r.x = 0;
+	r.y = 0;
+	r.w = size;
+	r.h = size; 
+}
 
 int main(int argc, char** args) {
-
 	SDL_Window* window;
 	SDL_Renderer* renderer;
 	SDL_Surface* surface;
 	SDL_Texture* texture;
 
-	if (SDL_Init(SDL_INIT_VIDEO)) {
-		SDL_Log("Error initializing SDL%s", SDL_GetError());
-		exit(1);
-	}
+	if (SDL_Init(SDL_INIT_VIDEO)) { SDL_Log("Error SDL init%s", SDL_GetError()); exit(1);}
 
 	SDL_Log("SDL initialized");
 	
@@ -19,27 +32,30 @@ int main(int argc, char** args) {
 		exit(1);
 	}
 
-	SDL_Event event;
+	SDL_Texture* a_tex = loadImageIntoTexture("../images/font.bmp", renderer);
+	if (!a_tex) { SDL_Log("Error: tex loading %s", SDL_GetError()); exit(1); }
+
+	int cursorX=0, cursorY=0;
+	SDL_StartTextInput();
 	while (1) {
-		SDL_PollEvent(&event);
+		SDL_Event event;
+		//SDL_PollEvent(&event);
+		SDL_WaitEvent(&event);
 		if (event.type == SDL_QUIT) break;
+		if (event.type == SDL_TEXTINPUT) printf("textinput: %s\n", event.text.text);
+		if (event.type == SDL_KEYUP) printf("keyup: %d\n", event.key.keysym.sym);
+		if (event.type == SDL_TEXTEDITING) printf("textedit: %s\n", event.edit.text);
 
 		SDL_SetRenderDrawColor(renderer, 0x20, 0x00, 0x00, 0xFF);
 		SDL_RenderClear(renderer);
 		SDL_SetRenderDrawColor(renderer, 0xee, 0xea, 0xee, 0xFF);
-		for (int x = 0; x < 200; x++) {
-			for (int y = 0; y < 150; y++) {
-				SDL_RenderDrawPoint(renderer, x, y);
-			}
-		}
+		SDL_Rect r; fillRect(r, 32);
+		SDL_RenderCopy(renderer, a_tex, NULL, &r);
 		SDL_RenderPresent(renderer);
-
 	}
 
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
-
-	
 	
 	return 0;
 }
